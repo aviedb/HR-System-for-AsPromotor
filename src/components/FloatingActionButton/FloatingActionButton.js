7fafc6bce6b13e55ca49b4312d5db4dbaafd7b47
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2019-07-12 16:24:09 +0700

    Refine Fab

--- src/components/FloatingActionButton/FloatingActionButton.js
+++ src/components/FloatingActionButton/FloatingActionButton.js
@@ -3,14 +3,10 @@ import { TouchableHighlight as TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-class FloatingActionButton extends Component {
-  render() {
-    return (
-      <TouchableOpacity {...this.props} style={styles.fab} underlayColor="rgba(50, 103, 255, .7)">
-        {this.props.children}
-      </TouchableOpacity>
-    )
-  }
-}
+const FloatingActionButton = (props) => (
+  <TouchableOpacity {...props} style={styles.fab} underlayColor="rgba(50, 103, 255, .7)">
+    {props.children}
+  </TouchableOpacity>
+);
 
 export default FloatingActionButton;
