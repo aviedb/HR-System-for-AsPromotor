011bfd6e537547db0727cf311c347ffce31ac34f
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2019-07-22 16:42:03 +0700

    Remove unnecessary

--- src/views/AddReport/styles.js
+++ src/views/AddReport/styles.js
@@ -55,32 +55,6 @@ const styles = StyleSheet.create({
     borderTopColor: '#EAEEF1',
     borderTopWidth: 1
   },
-  bottomSheetModal: {
-    margin: 0, 
-    justifyContent: 'flex-end'
-  },
-  bottomSheetContainer: {
-    backgroundColor: 'white',
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
-    paddingBottom: 12
-  },
-  bottomSheetHandle: {
-    alignSelf: 'center',
-    width: 40,
-    height: 4,
-    backgroundColor: theme["background-basic-color-3"],
-    borderRadius: 50,
-    marginTop: 12,
-    marginBottom: 12
-  },
-  bottomSheetTitle: {
-    color: theme["text-basic-color"],
-    fontWeight: 'normal',
-    fontFamily: 'product_sans_medium',
-    fontSize: 16,
-    alignSelf: 'center'
-  },
   bottomSheetItem: {
     flexDirection: 'row',
     paddingVertical: 12,
