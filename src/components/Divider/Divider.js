c78114f382acbe5059630843ca70715842549f14
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2019-07-26 16:06:31 +0700

    Update Divider component

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -6,10 +6,11 @@ const Divider = (props) => {
   const backgroundColor = props.color || theme["border-basic-color-3"];
   const marginTop = props.marginTop || 0;
   const marginBottom = props.marginBottom || 0;
+  const height = props.height || 1;
 
   return (
     <View style={{ 
-      height: 1, 
+      height, 
       backgroundColor,
       marginTop,
       marginBottom
