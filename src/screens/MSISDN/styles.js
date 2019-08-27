e9b5e8e1a555b90b667d7e4ca06c901b1e3d26f5
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2019-08-27 15:19:14 +0700

    Fix MSISDN top -1

--- src/screens/MSISDN/styles.js
+++ src/screens/MSISDN/styles.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme["background-basic-color-2"],
-    top: -1
+    marginTop: -1
   },
   item: {
     borderTopColor: theme["border-basic-color-2"],
